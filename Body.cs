90f33e5c1771d9e6829a2a16f0cd1342acccde4f
Ron Tseytlin <ronts390@gmail.com>
2021-03-19 18:56:22 +0200

    added body class (but no interaction yet)

---
+++ Body.cs
@@ -0,0 +1,22 @@
+﻿namespace MultithreadedGravity
+{
+    struct Body
+    {
+        public double x;
+        public double y;
+        public double velX;
+        public double velY;
+        public double mass;
+
+        public void Interaction(Body other)
+        {
+            //TODO
+        }
+
+        public void Move()
+        {
+            x += velX;
+            y += velY;
+        }
+    }
+}
